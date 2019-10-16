dec21a3d7d7bffd484570cc2c058a2d32fad19fd
Jared Chavez <jared@undeadlabs.com>
2019-10-16 14:39:47 -0700

    oops, fix call stack depth and log type labels

--- ConsoleLogger.go
+++ ConsoleLogger.go
@@ -9,15 +9,15 @@ var ConsoleLogger = &ConsoleLog{}
 type ConsoleLog struct{}
 
 func (nl *ConsoleLog) Debug(format string, v ...interface{}) {
-	nl.Print(NewLogMsg("info", format, 3, v...))
+	nl.Print(NewLogMsg("debug", format, 2, v...))
 }
 
 func (nl *ConsoleLog) Error(format string, v ...interface{}) {
-	nl.Print(NewLogMsg("info", format, 3, v...))
+	nl.Print(NewLogMsg("error", format, 2, v...))
 }
 
 func (nl *ConsoleLog) Info(format string, v ...interface{}) {
-	nl.Print(NewLogMsg("info", format, 3, v...))
+	nl.Print(NewLogMsg("info", format, 2, v...))
 }
 
 func (nl *ConsoleLog) Print(msg *LogMsg) {
